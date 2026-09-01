9615a530a3df84cf5b9e1c2e0c26dac767c92fa8
abdulqadirbhagat8-del <abdulqadirbhagat8@gmail.com>
2026-09-01 18:09:18 +0530

    Update website_logout.php to clear persistent login tokens (Requirements #6 and #9)

--- website_logout.php
+++ website_logout.php
@@ -2,19 +2,56 @@
 
 session_start();
 
+require_once "config.php";
+require_once "session_manager.php";
+
 /*
 ==================================================
 WEBSITE MEMBER LOGOUT
 ==================================================
 */
 
+/* Get member ID before destroying session */
+
+$member_id = $_SESSION['member_id'] ?? null;
+
 /* Remove member session */
 
 unset($_SESSION['member_id']);
+unset($_SESSION['member_its_id']);
+unset($_SESSION['member_name']);
+unset($_SESSION['member_logged_in']);
 
 /* Destroy the complete session */
 
 session_destroy();
+
+/*
+==================================================
+REVOKE PERSISTENT LOGIN TOKEN
+==================================================
+*/
+
+if ($member_id) {
+
+    $sessionManager = new SessionManager($conn);
+    
+    /* Revoke all login tokens for this member */
+    $sessionManager->revokeAllTokens($member_id);
+
+}
+
+/* Clear login token cookie */
+
+setcookie(
+    "login_token",
+    "",
+    time() - 3600,
+    "/",
+    "",
+    isset($_SERVER["HTTPS"]),
+    true
+);
 
 /* Redirect to Member Website Login */
 
